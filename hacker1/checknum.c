2cdac7868a485d7d9a891fb18668717173bc8345
phnsci <phn10@case.edu>
2016-12-23 08:16:40 -0500

    add getLongLong function to chechknum.c

--- hacker1/checknum.c
+++ hacker1/checknum.c
@@ -63,6 +63,48 @@ int getInt() {
 	return atoi(buffer);
 }
 
+
+/**
+	check if user enter a valid non-negative number
+	@return an integer if valid input, a 0 if invalid
+*/
+long long getLongLong() {
+	char ch;											// handle individual user input
+	char char_count = 0;					// stdin buffer length
+
+	ch = getchar();								// get char from stdin buffer
+
+	// return invalid if user enter nothing	
+	if (ch == '\n')
+	{
+		buffer[0] = 0x00;
+		return 0;
+	}
+
+	// iterate through stdin buffer 
+	while ((ch != '\n') && (char_count < MAXBUFFERSIZE)) 
+	{
+		// append to buffer string if the input is valid
+		if ((ch >= '0') && (ch <= '9'))
+		{
+			buffer[char_count++] = ch;
+			ch = getchar();
+		}
+		// if not, clear stdin buffer and return 0
+		else
+		{
+			buffer[0] = 0x00;
+			clearBuffer();
+			return 0;
+		}
+	}
+
+	// null terminate buffer at the end
+	buffer[char_count] = 0x00;
+	
+	return atoll(buffer);
+}
+
 /**
 	check if user enter a valid non-negative float
 	@return the float if valid, 0 if not valid
