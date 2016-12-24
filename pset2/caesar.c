d8ee593f64449bf06a02e2828219920584c34e8f
phnsci <phn10@case.edu>
2016-12-24 18:13:27 -0500

    add isNumber function in caesar.c

--- pset2/caesar.c
+++ pset2/caesar.c
@@ -3,14 +3,21 @@
 #include <string.h>
 #include "mylib.h"
 
+typedef int bool;
+#define true 1
+#define false 0
+
+bool isNumber(char *);
+
 int main(int argc, char* argv[])
 {
 	int k = 0;			// caesar key
 	char *arr;			// string 
 
-	// check command line argument
-	if (argc != 2)
-		return 1;
+	// check number of command line arguments
+	if (argc != 2) 					return 1;
+	if (!isNumber(argv[1])) return 1;
+
 
 	// get key from command line
 	k = atoi(argv[1]);
@@ -35,4 +42,16 @@ int main(int argc, char* argv[])
 	printf("%s\n", arr);
 			
 	return 0;
+}
+
+bool isNumber(char *arr)
+{
+	int i = 0;
+	while (i < strlen(arr))
+	{
+		if (!((arr[i] >= '0') && (arr[i] <= '9')))
+			return false;
+		i++;
+	}
+	return true;
 }
