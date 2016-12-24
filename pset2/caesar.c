0c0e3ab71aeb8dec5ef329305093a03f9c90182b
phnsci <phn10@case.edu>
2016-12-24 17:49:22 -0500

    initial commit pset2

---
+++ pset2/caesar.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "mylib.h"
+
+int testValid(char *); 
+
+int main(int argc, char* argv[])
+{
+	int k = 0;			// caesar key
+	char *arr;			// string 
+
+	// check command line argument
+	if (argc != 2)
+		return 1;
+
+	// get key from command line
+	k = atoi(argv[1]);
+
+	// get input string
+	do 
+	{
+		arr = getString();
+	} while (arr == NULL);
+
+	// iterate through each characters in string
+	for (int i = 0; i < strlen(arr); i++)
+	{
+		// if character is lowercase
+		if (arr[i] >= 'a' && arr[i] <= 'z')
+			arr[i] = 'a' + (arr[i] + k - 'a') % 26;
+		// if character is uppercase
+		if (arr[i] >= 'A' && arr[i] <= 'Z')
+			arr[i] = 'A' + (arr[i] + k - 'A') % 26;
+	}
+
+	printf("%s\n", arr);
+			
+	return 0;
+}
---
+++ pset2/mylib.h
@@ -0,0 +1,16 @@
+#ifndef CHECKNUM_H_
+#define CHECKNUM_H_
+
+void clearbuffer();
+
+char* checkNum();
+
+int getInt();
+
+long long getLongLong();
+
+float getFloat();
+
+char* getString();
+
+#endif 
