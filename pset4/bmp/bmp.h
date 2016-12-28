7f44c5668f8c05d952c275c17858801706b39345
phnsci <phn10@case.edu>
2016-12-28 13:00:33 -0500

    add whodunit.c and fix indent in bmp.h

--- pset4/bmp/bmp.h
+++ pset4/bmp/bmp.h
@@ -33,11 +33,11 @@ typedef uint16_t WORD;
  */
 typedef struct 
 { 
-    WORD   bfType; 
-    DWORD  bfSize; 
-    WORD   bfReserved1; 
-    WORD   bfReserved2; 
-    DWORD  bfOffBits; 
+	WORD   bfType; 
+	DWORD  bfSize; 
+	WORD   bfReserved1; 
+	WORD   bfReserved2; 
+	DWORD  bfOffBits; 
 } __attribute__((__packed__)) 
 BITMAPFILEHEADER; 
 
@@ -51,17 +51,17 @@ BITMAPFILEHEADER;
  */
 typedef struct
 {
-    DWORD  biSize; 
-    LONG   biWidth; 
-    LONG   biHeight; 
-    WORD   biPlanes; 
-    WORD   biBitCount; 
-    DWORD  biCompression; 
-    DWORD  biSizeImage; 
-    LONG   biXPelsPerMeter; 
-    LONG   biYPelsPerMeter; 
-    DWORD  biClrUsed; 
-    DWORD  biClrImportant; 
+	DWORD  biSize; 
+	LONG   biWidth; 
+	LONG   biHeight; 
+	WORD   biPlanes; 
+	WORD   biBitCount; 
+	DWORD  biCompression; 
+	DWORD  biSizeImage; 
+	LONG   biXPelsPerMeter; 
+	LONG   biYPelsPerMeter; 
+	DWORD  biClrUsed; 
+	DWORD  biClrImportant; 
 } __attribute__((__packed__))
 BITMAPINFOHEADER; 
 
@@ -75,8 +75,8 @@ BITMAPINFOHEADER;
  */
 typedef struct
 {
-    BYTE  rgbtBlue;
-    BYTE  rgbtGreen;
-    BYTE  rgbtRed;
+	BYTE  rgbtBlue;
+	BYTE  rgbtGreen;
+	BYTE  rgbtRed;
 } __attribute__((__packed__))
 RGBTRIPLE;
